9c3197b25787d78859af24e007dc6a2497d803cb
Atin Purwar <purwaratin777@gmail.com>
2026-02-20 03:44:12 +0530

    folder-01 done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+ 
+
+const App = () => {
+  return (
+   <>
+   <div id='Parent'>
+    <h1>Hello</h1>
+   </div>
+   <div id='chacha'>
+    <h2>world</h2>
+   </div>
+   </>
+  )
+}
+
+export default App  
