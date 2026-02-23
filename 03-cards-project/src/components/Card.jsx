196f6b6812eb6a1134361640274d25264b01979b
Atin Purwar <purwaratin777@gmail.com>
2026-02-24 03:52:17 +0530

    Completed my cards project

---
+++ 03-cards-project/src/components/Card.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Bookmark} from 'lucide-react'
+
+const card = (props) => {
+  console.log(props)
+  return (
+    // <div className='parent'>
+        <div className="card">
+        <div><div className='top'>
+            <img src={props.img} alt="Logo" />
+            <button>Save <Bookmark size={12} /></button>
+
+        </div>
+
+
+        <div className='center'>
+            <h3>{props.company} <span>{props.date}</span></h3>
+            <h2>{props.post}</h2>
+            <div className='tag'>
+                <h4>{props.tag1}</h4>
+                <h4>{props.tag2}</h4>
+            </div>
+        </div></div>
+
+
+        <div className='bottom'>
+            <div>
+                <h3>${props.hour}/-hour</h3>
+            <p>{props.location}</p>
+            </div>
+           <button>Apply</button>
+        </div>     
+                
+      </div>
+     
+      // </div>
+   
+  )
+}
+
+export default card
---
+++ 03-cards-project/src/components/User.jsx
@@ -0,0 +1,116 @@
+import React from 'react'
+
+const User = () => {
+
+    const jobOpenings = [
+  {
+    brandLogo: "https://logo.clearbit.com/google.com",
+    companyName: "Google",
+    datePosted: "5 days ago",
+    post: "Frontend Developer",
+    tag1: "Full Time",
+    tag3: "Junior Level",
+    payPerHour: 45,
+    location: "Mumbai"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/apple.com",
+    companyName: "Apple",
+    datePosted: "2 weeks ago",
+    post: "iOS Developer",
+    tag1: "Full Time",
+    tag3: "Mid Level",
+    payPerHour: 55,
+    location: "Delhi"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/meta.com",
+    companyName: "Meta",
+    datePosted: "10 days ago",
+    post: "React Developer",
+    tag1: "Remote",
+    tag3: "Senior Level",
+    payPerHour: 60,
+    location: "Bangalore"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/amazon.com",
+    companyName: "Amazon",
+    datePosted: "3 weeks ago",
+    post: "Backend Developer",
+    tag1: "Full Time",
+    tag3: "Junior Level",
+    payPerHour: 40,
+    location: "Hyderabad"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/netflix.com",
+    companyName: "Netflix",
+    datePosted: "1 week ago",
+    post: "UI/UX Engineer",
+    tag1: "Contract",
+    tag3: "Senior Level",
+    payPerHour: 65,
+    location: "Pune"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/microsoft.com",
+    companyName: "Microsoft",
+    datePosted: "4 days ago",
+    post: "Cloud Engineer",
+    tag1: "Full Time",
+    tag3: "Mid Level",
+    payPerHour: 50,
+    location: "Noida"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/tesla.com",
+    companyName: "Tesla",
+    datePosted: "6 days ago",
+    post: "Embedded Systems Engineer",
+    tag1: "Full Time",
+    tag3: "Senior Level",
+    payPerHour: 70,
+    location: "Chennai"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/adobe.com",
+    companyName: "Adobe",
+    datePosted: "8 days ago",
+    post: "Full Stack Developer",
+    tag1: "Part Time",
+    tag3: "Junior Level",
+    payPerHour: 38,
+    location: "Gurgaon"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/uber.com",
+    companyName: "Uber",
+    datePosted: "12 days ago",
+    post: "Data Engineer",
+    tag1: "Full Time",
+    tag3: "Mid Level",
+    payPerHour: 58,
+    location: "Mumbai"
+  },
+  {
+    brandLogo: "https://logo.clearbit.com/airbnb.com",
+    companyName: "Airbnb",
+    datePosted: "10 weeks ago",
+    post: "Software Engineer",
+    tag1: "Remote",
+    tag3: "Senior Level",
+    payPerHour: 62,
+    location: "Delhi"
+  }
+];
+console.log(jobOpenings)
+  return (
+    <div className='parent'>
+
+      
+    </div>
+  )
+}
+
+export default User
